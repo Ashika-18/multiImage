6ec0b32954a3e22e3ee4f0780b6235c1f011d2e1
Ashika-18 <ashibasama@gmail.com>
2023-07-13 18:47:37 +0900

    blurぼかし加工

--- script.js
+++ script.js
@@ -1,5 +1,21 @@
+const items = document.querySelectorAll('.img-item');
 
+items.forEach((item, index) => {
+    const keyframes = {
+        filter: ['blur(20px)', 'blur(0)'],
+        // opacity: [0, 1],
+        // rotate: ['10deg', 0],
+        // scale: [2, 1],
+        // translate: ['0 100px', 0],
+    };
+    const options = {
+        duration: 600,
+        fill: 'forwards',
+        delay: index * 300,
+    };
 
+    item.animate(keyframes, options);
+});
 
 //準備完了
 console.log('-STANDBY-')
